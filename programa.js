6c8ac1ff5cc3fc47b12a512584f4bbc251e1f9aa
logan6230 <pantoja2187@gmail.com>
2023-03-08 07:50:10 -0500

    Agregar funcion sacar promedio

--- programa.js
+++ programa.js
@@ -7,14 +7,10 @@ calificación se compone de los siguientes porcentajes:
  */
 
 function calcular_nota() {
-    let nota_1 = parseFloat(document.getElementById('nota_1').value);
-    let nota_2 = parseFloat(document.getElementById('nota_2').value);
-    let nota_3 = parseFloat(document.getElementById('nota_3').value);
-
     let examen_final = parseFloat(document.getElementById('trabajo_final').value);
     let trabajo_final = parseFloat(document.getElementById('examen_final').value);
-    let promedio_notas = nota_1 + nota_2 + nota_3
-    let porcentaje_notas = ((promedio_notas / 3) * 55) / 100;
+    let porcentaje_notas = sacar_promedio();
+        
     let porcentaje_examen = (examen_final * 30) / 100;
     let porcentaje_trabajo = (trabajo_final * 15) / 100;
     let nota_final = porcentaje_notas + porcentaje_examen + porcentaje_trabajo;
@@ -25,6 +21,16 @@ function calcular_nota() {
         nota.innerHTML = '<p style="color:green;font-size: 20px;">' +'<b>'+ `La nota final del alumno es -> ${nota_final}` + '</p>';
     } else {
         nota.innerHTML = '<p style="color:red;">' + "Debe ingresar todos los datos requeridos" + '</p>';
+    }
+
+    function sacar_promedio(){
+        let nota_1 = parseFloat(document.getElementById('nota_1').value);
+        let nota_2 = parseFloat(document.getElementById('nota_2').value);
+        let nota_3 = parseFloat(document.getElementById('nota_3').value);
+        let promedio_notas = nota_1 + nota_2 + nota_3;
+        let porcentaje_notas = (promedio_notas / 3) * 0.55;
+
+        return porcentaje_notas
     }
 
 }
